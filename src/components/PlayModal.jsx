c1fb4d4b75ec2a18b18aa8bcf8b9c8526df30292
bokeum <bokmail83@gmail.com>
2025-02-14 16:43:53 +0900

    convert mp4 to gif

--- src/components/PlayModal.jsx
+++ src/components/PlayModal.jsx
@@ -1,15 +1,12 @@
 // src/components/PlayModal.jsx
-
 import styles from '../styles/PlayModal.module.css';
 
 function PlayModal({ onClose }) {
   return (
     <div className={styles.modalOverlay}>
       <div className={styles.modalContent}>
-        <video className={styles.videoPlayer} controls autoPlay>
-          <source src="/assets/play.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
+        {/* MP4 대신 GIF 이미지 사용 */}
+        <img src="/assets/play.gif" alt="Play Animation" className={styles.playGif} />
         <button className={styles.closeButton} onClick={onClose}>
           종료
         </button>
